d314881e92d62034754a726eb48439e6fa136277
Geert Beuneker <geert@wikkl.me>
2023-03-31 17:10:37 +0200

    Fixed videolist responsiveness

--- src/components/organisms/video-list.svelte
+++ src/components/organisms/video-list.svelte
@@ -5,7 +5,7 @@
   export let videos: CollectionEntry<"video">[] = [];
 </script>
 
-<div class="grid grid-cols-3">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
   {#each videos as video}
     <VideoCard {video} />
   {/each}
