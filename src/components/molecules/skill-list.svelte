f6cfef755daefa4128190fc42aab567a6343d21d
Geert Beuneker <geert@wikkl.me>
2023-04-02 22:52:33 +0200

    Implemented skill bars

--- src/components/molecules/skill-list.svelte
+++ src/components/molecules/skill-list.svelte
@@ -9,14 +9,20 @@
   <span>Proficiency</span>
 </div>
 <hr class="mb-2" />
+<div class="ml-36 grid grid-cols-5 place-items-center sm:text-base text-[0.6rem]">
+  <span>Bad</span>
+  <span>Poor</span>
+  <span>Fair</span>
+  <span>Good</span>
+  <span>Excellent</span>
+</div>
+
 {#each skills.sort((a, b) => b.fill - a.fill) as skill}
   <div class="flex items-center">
-    <p class="w-36 text-xl">{skill.label}</p>
+    <p class="w-36">{skill.label}</p>
 
-    <span>Low</span>
-    <div class="mx-1 w-full">
-      <ProgressBar fill={skill.fill} />
+    <div class="mx-1 flex-grow">
+      <ProgressBar fill={skill.fill} segments={6} />
     </div>
-    <span>High</span>
   </div>
 {/each}
--- src/components/organisms/about-details.svelte
+++ src/components/organisms/about-details.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import ProgressBar from "../atoms/progress-bar.svelte";
   import SkillList from "../molecules/skill-list.svelte";
+  import About from "./about.svelte";
+  import Section from "./section.svelte";
 
   const programmingSkills: { label: string; fill: number }[] = [
     {
@@ -17,16 +18,27 @@
     },
     {
       label: "C++",
-      fill: 0.6,
+      fill: 0.55,
     },
     {
       label: "Python",
-      fill: 0.4,
+      fill: 0.44,
     },
-
+    {
+      label: "SQL",
+      fill: 0.42,
+    },
     {
       label: "PHP",
-      fill: 0.2,
+      fill: 0.35,
+    },
+    {
+      label: "HLSL",
+      fill: 0.3,
+    },
+    {
+      label: "Haskell",
+      fill: 0.1,
     },
   ];
 
@@ -36,8 +48,20 @@
       fill: 1,
     },
     {
-      label: "React",
+      label: "Unity",
+      fill: 1,
+    },
+    {
+      label: "BabylonJS",
       fill: 0.7,
+    },
+    {
+      label: "PixiJS",
+      fill: 0.7,
+    },
+    {
+      label: "React",
+      fill: 0.5,
     },
     {
       label: "Vue",
@@ -45,59 +69,85 @@
     },
     {
       label: "Svelte",
-      fill: 0.5,
-    },
-    {
-      label: "Unity",
-      fill: 1,
+      fill: 0.45,
     },
     {
       label: "Astro",
-      fill: 0.4,
+      fill: 0.45,
+    },
+    {
+      label: "Game Maker Studio",
+      fill: 0.35,
+    },
+    {
+      label: "Unreal Engine",
+      fill: 0.1,
     },
   ];
 
   const software: { label: string; fill: number }[] = [
     {
-      label: "Adobe Premiere",
-      fill: 0.8,
-    },
-    {
-      label: "Adobe After Effects",
-      fill: 0.8,
-    },
-    {
       label: "Adobe Photoshop",
-      fill: 0.5,
-    },
-    {
-      label: "Adobe Illustrator",
-      fill: 0.8,
-    },
-    {
-      label: "Blender",
-      fill: 0.8,
+      fill: 0.85,
     },
     {
       label: "DaVinci Resolve",
-      fill: 0.2,
+      fill: 0.78,
+    },
+    {
+      label: "Adobe Premiere",
+      fill: 0.78,
+    },
+    {
+      label: "Adobe After Effects",
+      fill: 0.75,
+    },
+    {
+      label: "Adobe Illustrator",
+      fill: 0.62,
+    },
+    {
+      label: "Blender",
+      fill: 0.5,
     },
   ];
 </script>
 
-<div>
-  <p class="mb-10">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod enim amet odio ea aliquam adipisci fugit, tenetur
-    excepturi recusandae, ipsa exercitationem voluptatem labore, aperiam facilis perferendis ipsum. Iure, minus
-    repudiandae?
+<div class="max-w-3xl mx-auto">
+  <p>
+    When you're looking for interactive experiences, games, tools, or websites that meet your specific needs and
+    preferences, you can count on Shellpad Interactive. We are a company that was founded in 2017 with the aim of
+    providing high-quality products and services that are modular, scalable, and customized to meet your unique
+    requirements.
   </p>
-
-  <h3 class="my-5">Programming</h3>
-  <SkillList skills={programmingSkills} />
-
-  <h3 class="my-5">Frameworks/Engines</h3>
-  <SkillList skills={frameworks} />
-
-  <h3 class="my-5">Software</h3>
-  <SkillList skills={software} />
+  <p>
+    We take pride in our academic background in Computer Science, as well as our extensive experience in the field. With
+    our expertise, we work closely with you to ensure that you receive the product that is tailored to your needs.
+  </p>
+  <p>
+    At Shellpad Interactive, we are dedicated to delivering interactive experiences that will capture your imagination
+    and provide you with an unparalleled level of entertainment. You can rely on us to exceed your expectations and make
+    your vision a reality.
+  </p>
 </div>
+
+<Section title="Skills">
+  <div>
+    <div class="max-w-3xl mx-auto">
+      <h3 class="my-5">Programming</h3>
+      <SkillList skills={programmingSkills} />
+
+      <h3 class="my-5">Frameworks and Engines</h3>
+      <SkillList skills={frameworks} />
+
+      <h3 class="my-5">Software</h3>
+      <SkillList skills={software} />
+    </div>
+  </div>
+</Section>
+
+<style lang="scss">
+  p {
+    @apply mb-5;
+  }
+</style>
