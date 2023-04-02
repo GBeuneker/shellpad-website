ea454e6f98a588a969677ed22415fa9a6bb60c55
Geert Beuneker <geert@wikkl.me>
2023-04-02 23:08:53 +0200

    Fixed navbar active class

--- src/components/organisms/navbar.svelte
+++ src/components/organisms/navbar.svelte
@@ -1,12 +1,24 @@
+<script>
+  export let url = "";
+</script>
+
 <nav class="fixed top-0 w-full flex gap-2 justify-center text-xl bg-light text-dark shadow py-2 z-50">
-  <a href="/">Home</a>
-  <a href="/projects">Projects</a>
-  <a href="/about">About</a>
+  <a href="/" class:active={url === "/"}>Home</a>
+  <a
+    href="/projects"
+    class:active={url.includes("/projects") ||
+      url.includes("/game") ||
+      url.includes("/web") ||
+      url.includes("/software") ||
+      url.includes("/video")}>Projects</a
+  >
+  <a href="/about" class:active={url.includes("/about")}>About</a>
 </nav>
 
 <style lang="scss">
   a {
     @apply transition-all font-bold;
+    &.active,
     &:hover {
       @apply text-primary;
     }
