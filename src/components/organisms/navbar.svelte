fa9dbd5ea222bec10a8d088469771aacfe3ee761
Geert Beuneker <geert@wikkl.me>
2023-04-02 23:03:16 +0200

    Added navbar

--- src/components/organisms/navbar.svelte
+++ src/components/organisms/navbar.svelte
@@ -1,5 +1,14 @@
-<nav>
+<nav class="fixed top-0 w-full flex gap-2 justify-center text-xl bg-light text-dark shadow py-2 z-50">
   <a href="/">Home</a>
-  <a href="/blog">Blog</a>
-  <a href="https://github.com/withastro/astro" target="_blank">GitHub</a>
+  <a href="/projects">Projects</a>
+  <a href="/about">About</a>
 </nav>
+
+<style lang="scss">
+  a {
+    @apply transition-all font-bold;
+    &:hover {
+      @apply text-primary;
+    }
+  }
+</style>
